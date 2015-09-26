86972a365036153831dcd29eea9174532be4080d
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2015-09-26 14:17:00 +1000

    Init

---
+++ payout.js
@@ -0,0 +1,54 @@
+
+
+
+if (Meteor.isClient) {
+  Router.route('/', function () {
+    // render the Home template with a custom data context
+    this.render('Login', {data: {title: 'My Title'}});
+  });
+
+  // when you navigate to "/one" automatically render the template named "One".
+  Router.route('/one');
+
+  Template.Login.events({
+        'click #facebook-login': function(event) {
+          Meteor.loginWithFacebook({ loginStyle: "popup" }, function(err){
+              if (err) {
+                  throw new Meteor.Error("Facebook login failed");
+              } 
+          });
+      },
+   
+      'click #logout': function(event) {
+          Meteor.logout(function(err){
+              if (err) {
+                  throw new Meteor.Error("Logout faile  d");
+              }
+          })
+      }
+  });
+
+
+}
+
+if (Meteor.isServer) {
+
+
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+
+    ServiceConfiguration.configurations.upsert(
+      { service: "facebook" },
+      {
+        $set: {
+          clientId: "62a7cba58da60cc1f850c2b95b0638c4",
+          loginStyle: "popup",
+          appId: '442721019253710',
+          secret: '1dec7c127e7a69decf46f3fcf4a37aff'
+        }
+      }
+    );
+
+  });
+}
